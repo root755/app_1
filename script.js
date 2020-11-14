55a8011192507457e6ae7211c81a9d60c1bfd037
root755 <example@gmail.com>
2020-11-14 16:02:07 +0300

    add personalMovieDB

--- script.js
+++ script.js
@@ -2,4 +2,14 @@
 
 const numberOfFilms = prompt("Сколько Фильмов Вы уже посмотрели?", "");
 
-console.log (numberOfFilms);
+const obj1;
+const obj2;
+let arr = [];
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: obj1,
+    actors: obj2,
+    genres: arr,
+    private: false
+};
